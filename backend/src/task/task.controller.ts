8f25cf6ea8c549af9707d9c8018382ad7e13587a
russellandrady <russellsuper2000@gmail.com>
2025-10-22 23:00:37 +0530

    api endpoints implemented

--- backend/src/task/task.controller.ts
+++ backend/src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, Query } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { executeService } from 'src/common/utils/execute-service.util';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -10,5 +10,27 @@ export class TaskController {
   @Post()
   createTask(@Body() dto: CreateTaskDto) {
     return executeService(() => this.taskService.createTask(dto) );
+  }
+
+  @Patch(':id')
+  markTaskAsCompleted(@Param('id') id: string) {
+    return executeService(() => this.taskService.markTaskAsCompleted(Number(id)));
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    return executeService(() => this.taskService.deleteTask(Number(id)));
+  }
+
+  @Get('completed')
+  getCompletedTasks(@Query('page') page: string) {
+    const pageNumber = page ? Number(page) : 1;
+    return executeService(() => this.taskService.getTasks(pageNumber, true));
+  }
+
+  @Get('not-completed')
+  getNotCompletedTasks(@Query('page') page: string) {
+    const pageNumber = page ? Number(page) : 1;
+    return executeService(() => this.taskService.getTasks(pageNumber, false));
   }
 }
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -3,11 +3,47 @@ import { PrismaService } from '../prisma/prisma.service';
 import { Task } from '@prisma/client';
 import { CreateTaskDto } from './dto/create-task.dto';
 
+
+export interface TaskResponse {
+  tasks: Task[];
+  total: number;
+}
 @Injectable()
 export class TaskService {
+  
   constructor(private prisma: PrismaService) {}
 
   async createTask(dto: CreateTaskDto): Promise<Task> {
     return this.prisma.task.create({ data: dto });
+  }
+
+  async markTaskAsCompleted(id: number): Promise<Task> {
+    return this.prisma.task.update({
+      where: { id },
+      data: { completed: true },
+    });
+  }
+
+  async deleteTask(id: number): Promise<Task> {
+    return this.prisma.task.delete({
+      where: { id },
+    });
+  }
+
+  async getTasks(page: number, completed: boolean): Promise<TaskResponse> {
+    const pageSize = 5;
+    const skip = (page - 1) * pageSize;
+
+    const [tasks, total] = await Promise.all([
+      this.prisma.task.findMany({
+        skip,
+        take: pageSize,
+        where: { completed },
+        orderBy: { createdAt: 'desc' },
+      }),
+      this.prisma.task.count({ where: { completed } }),
+    ]);
+
+    return { tasks, total };
   }
 }
